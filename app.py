038fd886170f4319b198211e7256d3f47ae170b1
Evaristo <evaripaulo@gmail.com>
2024-02-18 15:46:17 +0100

    add all the routes

--- app.py
+++ app.py
@@ -4,8 +4,22 @@ app = Flask(__name__)
 
 # HOMEPAGE
 @app.route('/')
+@app.route('/home')
+@app.route('/index')
 def home():
     return 'home'
+
+
+# HOMEPAGE
+@app.route('/about-us')
+def about():
+    return 'about-us'
+
+
+# HOMEPAGE
+@app.route('/contact-us')
+def contact():
+    return 'contact-us'
 
 
 if __name__ == '__main__':
