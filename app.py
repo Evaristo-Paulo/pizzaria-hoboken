da57428bd336684d565070944c99e51090e59851
Evaristo <evaripaulo@gmail.com>
2024-02-18 15:41:14 +0100

    add first route

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print('hello world')
+from flask import Flask
+
+app = Flask(__name__)
+
+# HOMEPAGE
+@app.route('/')
+def home():
+    return 'home'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
